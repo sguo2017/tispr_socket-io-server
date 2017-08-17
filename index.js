1611680569a4c14da7dab47fe6cbf15c8a10db18
zengkm <zengkm3@mail2.sysu.edu.cn>
2017-08-17 18:18:05 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 4000;
+
+var redis = require('redis');
+var Sidekiq = require('sidekiq');
+var redisClient = redis.createClient('/tmp/redis.sock');
+var sidekiq = new Sidekiq(redisClient);
+
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+var sockets = {};
+var thissocket
+io.on('connection', function (socket) {
+  console.log('connection');
+
+  socket.on('add user', function (senderId,deal_id) {
+    console.log('add user', senderId);
+    socket.sid= senderId;
+    socket.did= deal_id;
+    sockets[socket.sid] = socket; 
+  });
+
+  socket.on('new message', function (data) {
+    console.log('new message', data);
+
+    if (!sockets[data.recipientId]) {
+      console.log(data.recipientId, 'not online');
+    }else if(data.deal_id!= sockets[data.recipientId].did){
+      console.log(data.recipientId, 'not same order');
+    }else {
+      sockets[data.recipientId].emit('new message', {
+        senderId: data.senderId,
+        recipientId: data.recipientId,
+        message: data.message
+      });
+    }
+
+    saveMessage(data.chatRoomId, data.senderId, data.message);
+  });
+
+  socket.on('disconnect', function () {
+    console.log(socket.sid, 'disconnected');
+    delete sockets[socket.sid];
+  });
+});
+
+
+function saveMessage(cid, uid, msg) {
+  var options = {
+    queue: 'default'
+  };
+  sidekiq.enqueue("ChatWorker", [cid, uid, msg], options);
+}
